b93f54f1b874602bda811160f3cd73f49ab2e250
vgj24 <vrushlijoshi24@gmail.com>
2021-12-05 18:57:17 +0530

    added UC-9dict

---
+++ UC-9dict.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -x
+
+isPartTime=1
+isFullTime=2
+maxHoursInMonth=100
+empRatePerHour=20
+numOfWorkingDays=20
+totalEmpHr=0
+totalWorkingDays=0
+
+declare -A dailyWages;
+
+function getWorkingHours()
+{
+	case $1 in
+		   $isFullTime)
+			empHrs=8
+			;;
+		$isPartTime)
+			empHrs=4
+			;;
+			*)
+			empHrs=0
+			;;
+esac
+}
+
+function getEmpWage(){
+		echo $(($1*$empRatePerHour))
+}
+
+while [[ $totalEmpHr -lt $maxHoursInMonth 
+&& $totalWorkingDays -lt $numOfWorkingDays ]]
+do
+	((totalWorkingDays++))
+	empCheck=$((RANDOM%3))
+	getWorkingHours $empCheck
+	totalEmpHr=$(($totalEmpHr+$empHrs))
+#	dailyWages[$totalWorkingDays]=$(($empHrs+$empRatePerHour))
+	dailyWages["$totalWorkingDays"]=$(getEmpWage $empHrs)
+done
+
+
+#totalSalary="$( getEmpwage $totalEmpHr)"
+totalSalary=$(($totalEmpHr*$empRatePerHour))
+echo ${dailyWages[@]} #using array
+
+echo ${!dailyWages[@]}
